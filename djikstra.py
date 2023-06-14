79c0062dedaf0bb2abe3c817c944d559a8df994a
FelipeFelipeRenan <feliperenanqwerty@gmail.com>
2023-06-14 10:56:34 -0300

    adding comments to dijkstra

--- djikstra.py
+++ djikstra.py
@@ -8,13 +8,18 @@ def dijkstra(grafo, v1):
     # heap onde está a tupla (distancia, vertice)
     vertdist = [(0, v1)]
     
+    # enquanto a heap nao estiver vazia
     while vertdist:
+        # o vertice com menor distancia é retirado da heap
         distanciaAtual, verticeAtual = heapq.heappop(vertdist)
         
-        
+        # caso a distancia da iteração atual for maior que a presente no vetor de distancia
+        # segue para o proximo vertice
         if distanciaAtual > distancias[verticeAtual][0]:
             continue
-    
+        
+        # iteração emtre todos os vizinhos do vertice atual e calcula
+        # suas respectivas distancias
         for vizinho, peso in grafo[verticeAtual]:
             distancia = distanciaAtual + peso
 
@@ -22,10 +27,15 @@ def dijkstra(grafo, v1):
             if distancia < distancias[vizinho][0]:
                 distancias[vizinho] = [distancia, distancias[verticeAtual][1] + [vizinho]]
                 heapq.heappush(vertdist, (distancia, vizinho))
+    
+    # retorna um dicionario com as distancias
     return distancias
 
 
-
+# lista de adjacencias representada por um dicionario
+# contendo os vertices como chave uma lista composta por tuplas
+# representado os seus respectivos vizinhos e suas distancias para
+# o vertice na chava
 graph = {
     'A': [('B', 2), ('C', 4)],
     'B': [('A', 2), ('C', 1), ('D', 4)],
@@ -34,8 +44,8 @@ graph = {
 }
 
 # Executa o algoritmo de Dijkstra a partir do vértice A
-distances = dijkstra(graph, 'A')
+distancias = dijkstra(graph, 'A')
 
 # Imprime o resultado
-for vertex, (distance, path) in distances.items():
-    print(f"Menor caminho de A até {vertex}: {path}, com distância {distance}")
+for vertice, (distancia, caminho) in distancias.items():
+    print(f"Menor caminho de A até {vertice}: {caminho}, com distância {distancia}")
